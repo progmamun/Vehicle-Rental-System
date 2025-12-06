08101f5496452b69e1e40ed46fb243fb4da496dd
progmamun <mamun333089@gmail.com>
2025-12-06 23:47:16 +0600

    update

--- src/app/config/db.ts
+++ src/app/config/db.ts
@@ -1,39 +1,52 @@
 import { Pool } from 'pg';
 import config from '.';
 
-//DB
+// DB
 export const pool = new Pool({
   connectionString: `${config.connection_str}`,
 });
 
 const initDB = async () => {
   await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        role VARCHAR(50) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `);
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(150) UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      phone VARCHAR(20) NOT NULL,
+      role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer')),
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    );
+  `);
 
   await pool.query(`
-            CREATE TABLE IF NOT EXISTS todos(
-            id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(200) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            due_date DATE,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-            )
-            `);
+    CREATE TABLE IF NOT EXISTS vehicles (
+      id SERIAL PRIMARY KEY,
+      vehicle_name VARCHAR(150) NOT NULL,
+      type VARCHAR(50) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+      registration_number VARCHAR(100) UNIQUE NOT NULL,
+      daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price > 0),
+      availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked')),
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    );
+  `);
+
+  // BOOKINGS TABLE
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS bookings (
+      id SERIAL PRIMARY KEY,
+      customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+      vehicle_id INT NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE,
+      rent_start_date DATE NOT NULL,
+      rent_end_date DATE NOT NULL CHECK (rent_end_date > rent_start_date),
+      total_price NUMERIC(10,2) NOT NULL CHECK (total_price > 0),
+      status VARCHAR(20) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned')),
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    );
+  `);
 };
 
 export default initDB;
